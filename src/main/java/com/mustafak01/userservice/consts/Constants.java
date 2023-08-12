f61a8a5cc16bba4d66adb2d91affe35b2d6696fd
MustafaK01 <mestefe119@gmail.com>
2023-08-12 15:06:58 +0300

    add for constants

---
+++ src/main/java/com/mustafak01/userservice/consts/Constants.java
@@ -0,0 +1,9 @@
+package com.mustafak01.userservice.consts;
+
+public class Constants {
+    public static final String NEW_USER_ACCOUNT_VERIFICATION = "New User Account Verification";
+    public static final String USER_CREATED = "User Created";
+    public static final String ACCOUNT_VERIFIED = "Account Verified";
+    public static final String ALREADY_EXISTS="Already Exists";
+    public static final String MISSING_FIELDS = "Missing fields";
+}
